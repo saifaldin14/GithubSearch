9a367d8c0811f854ff9f2641e9648af14b87bed6
Saif Al-Din Ali <saifaldindeveloper@gmail.com>
2020-08-15 14:21:22 -0400

    Added project starter files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
